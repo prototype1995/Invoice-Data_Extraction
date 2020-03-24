582d51fc8044bafeb46f630974444436316a7048
prototype1995 <jak13595@gmail.com>
2020-03-24 14:39:40 +0530

    updations done for data idntfn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Detects invoice page from a pdf or image.
+# Retrieves data from it and returns a json object
 
 import word_retriever as wr
 import convert_pdf2image as pi
@@ -9,11 +10,16 @@ import os
 import json
 
 file_path = 'sample.pdf'  # give your pdf file path
-out_folder = 'output'
+out_folder = 'output'  # output folder path where converted images are stored.
 
 
 # Detection from pdf
 def detect_from_pdf():
+    '''
+    Method to convert PDF into images and identify invoice page from it.
+    Parameters: None
+    Returns: Image file name with path.
+    '''
     matching_loc_list = []
     invoice_detect_kwds = ['INVOICE', 'Invoice', 'Tax', 'TAX', 'Ship', 'Bill',
                            'Billing', 'Shipping', 'GSTIN', 'Registered',
@@ -45,6 +51,8 @@ invoice_file = detect_from_pdf()
 def value_retrieve(ret_val):
     '''
     Method to retrieve certain values and store in a dict.
+    Parameters: Value from which data is to be retrieved
+    Returns: Retrieve data.
     '''
     try:
         data = ret_val[0]
@@ -55,20 +63,24 @@ def value_retrieve(ret_val):
 
 
 def return_valid_data(inv_file):
+    '''
+    Method to fetch valid data from the invoice page with required details.
+    Parameters: image file with path
+    Returns: Valid data, dictionary with gst,mob,mail etc. values.
+    '''
     response, document, text = wr.data_retrieve(inv_file)
     # detection by block
     # bounds = wr.get_document_bounds(response, FeatureType.BLOCK, document)
     GST_values, MOB_nums, Mails = wr.reg_expn(text)
     check_data = GST_values
-    check_data.append('Registered')
-    # check_data.append('Office')
-    # must_data.append('Regn')
+    check_val = ('Registered', 'Regd', 'Pvt', 'Ltd')  # Add items to be identified
+    check_data.extend(check_val)
 
     data_coll = []
     invalid_data = []
     data_dict1 = {}  # dictionary contaning final valid data
     avoid_data = ['Adani', 'ADANI', 'Billing', 'Shipping', 'Bill', 'Ship'
-                  , 'Buyer']
+                  , 'Buyer', 'From', 'To']
     city_code_dict = {
                         '01': 'Jammu & Kashmir',
                         '02': 'Himachal Pradesh',
@@ -109,76 +121,119 @@ def return_valid_data(inv_file):
                         '37': 'Andhra Pradesh (New)'
     }
     for value in check_data:
-        # print(gst_value) # Debugging
+        # print(value)  # Debugging
         # state_code = gst_value[:2]
         # print(city_code_dict[state_code]) # Debugging
         location = wr.find_block_loc_from_word(document, value)
-        # print(location) # Debugging
-        # finding block wth containing word
-        data = wr.text_within(document, location.vertices[0].x
-                              , location.vertices[0].y, location.vertices[2].x
-                              , location.vertices[2].y)
-#        print('\n'+data) # Debugging
-        data_coll.append(data)
-    data_coll = set(data_coll)  # Done to avoid repetition
-    # print(data_coll)
-    for i in data_coll:
-        for j in avoid_data:
-            if i.find(j) != -1:
-                invalid_data.append(i)
-    invalid_data = set(invalid_data)  # avoid copies
-    # removing invalid data from data_coll
-    for data in invalid_data:
-        data_coll.remove(data)
-    for valid_data in data_coll:
-        # print(valid_data)
-        valid_GST_value, valid_MOB_num, valid_Mail = wr.reg_expn(valid_data)
-
-        if value_retrieve(valid_GST_value) == '' and value_retrieve(valid_MOB_num) == '' and value_retrieve(valid_Mail) == '':
+        if location is None:
             pass
         else:
-            data_dict1['GSTIN'] = value_retrieve(valid_GST_value)
-            data_dict1['Mob Num'] = value_retrieve(valid_MOB_num)
-            data_dict1['Email'] = value_retrieve(valid_Mail)
+            # print(location)  # Debugging
+            # finding block wth containing word
+            data = wr.text_within(document, location.vertices[0].x
+                                  , location.vertices[0].y, location.vertices[2].x
+                                  , location.vertices[2].y)
+    #        print('\n'+data) # Debugging
+            data_coll.append(data)
+        data_coll = set(data_coll)  # Done to avoid repetition
+        data_coll = list(data_coll)  # to list
+        # print(data_coll)
+        for i in data_coll:
+            for j in avoid_data:
+                if i.find(j) != -1:
+                    invalid_data.append(i)
+        invalid_data = set(invalid_data)  # avoid copies
+        invalid_data = list(invalid_data)  # to list
+        # removing invalid data from data_coll
+        for data in invalid_data:
             try:
-                data_dict1['Sate'] = city_code_dict[str(valid_GST_value[0])[:2]]
+                data_coll.remove(data)
             except:
-                data_dict1['Sate'] = ''
+                pass
+        for valid_data in data_coll:
+            # print(valid_data)
+            valid_GST_value, valid_MOB_num, valid_Mail = wr.reg_expn(valid_data)
+
+            if value_retrieve(valid_GST_value) == '' and value_retrieve(valid_MOB_num) == '' and value_retrieve(valid_Mail) == '':
+                pass
+            else:
+                data_dict1['GSTIN'] = value_retrieve(valid_GST_value)
+                data_dict1['Mob Num'] = value_retrieve(valid_MOB_num)
+                data_dict1['Email'] = value_retrieve(valid_Mail)
+                try:
+                    data_dict1['Sate'] = city_code_dict[str(valid_GST_value[0])[:2]]
+                except:
+                    data_dict1['Sate'] = ''
 
     return data_coll, data_dict1
+
+
+def find_vendor_name(data, vendor_kwd, typ=0):
+    '''
+    Method to find vendor name.
+    Here, typ denotes the type of data. 0-string ... 1-list
+    Parameters: source data, vendor identification keyword list, type of source data.
+    Returns: Vendor Name, Item containing vndr_name
+    '''
+    list_data = []
+    if typ == 0:
+        list_data.append(data)
+        data = list_data
+    else:
+        pass
+    vndr_name = ''
+    for i in data:
+        for k in vendor_kwd:
+            if i.find(k) != -1:
+                end_lim = i.find(k) + len(k)
+                vndr_name = i[:end_lim]
+                return vndr_name, i
+    # print('Vndr : '+vndr_name)  # Debugging
+    return vndr_name, ''
 
 
 def find_bal_data(data):
     '''
     Method to find balance data like Vendors Name , address etc.
+    Parameters: source data
+    Returns: dictionary with vndr_name, address etc.
     '''
     data_dict2 = {}
-    vendor_idntfn_kwd = ['Limited', 'Ltd', 'LTD', 'Private', 'PRIVATE'
+    vendor_idntfn_kwd = ['Limited', 'Ltd', 'LTD', 'Private', 'Pvt', 'PRIVATE'
                          , 'INC', 'Inc']
     pos = 1
     for i in data:
-        split_data = i.split(',')
-        for j in split_data:
-            for k in vendor_idntfn_kwd:
-                if j.find(k) != -1:
-                    end_lim = j.find(k) + len(k)
-                    data_dict2['Vendor Name'] = j[:end_lim]
-                    split_data.remove(j)
-                    break
-            data_dict2['Street'+str(pos)] = j
-            pos += 1
-
+        gst_value, mob_num, mail = wr.reg_expn(i)
+        data_dict2['Vendor Name'] = ''
+        if value_retrieve(gst_value) == '' and value_retrieve(mob_num) == '' and value_retrieve(mail) == '':
+            split_data = i.split(',')
+            data_dict2['Vendor Name'], rem_data = find_vendor_name(split_data, vendor_idntfn_kwd, 1)
+            # removing vendor name included item from list.
+            if data_dict2['Vendor Name'] != '':
+                try:
+                    split_data.remove(rem_data)
+                except:
+                    pass
+            for j in split_data:
+                data_dict2['Street'+str(pos)] = j
+                pos += 1
+        else:
+            if data_dict2['Vendor Name'] == '':
+                data_dict2['Vendor Name'], nonuse_data = find_vendor_name(i, vendor_idntfn_kwd, 0)
     return data_dict2
 
 
 def return_json(dict):
     '''
     Method to return a json file from a dictionary
+    Parameters: Merged dictionary
+    Returns: Converted json object.
     '''
     final_data = json.dumps(dict)
     return final_data
 
 
+# Retrieving Valid data as a list and Valid ditionary which contains gstin, mob, mail etc.
 valid_data_coll, valid_data_dict1 = return_valid_data(invoice_file)
 # print(valid_data_coll)  # Debugging
 # print(valid_data_dict1)  # Debugging
@@ -186,9 +241,10 @@ valid_data_coll, valid_data_dict1 = return_valid_data(invoice_file)
 valid_data_dict2 = find_bal_data(valid_data_coll)
 # print(valid_data_dict2)  # Debugging
 
-final_dict = {**valid_data_dict1, **valid_data_dict2} # Way to merge two dicts
+final_dict = {**valid_data_dict1, **valid_data_dict2}  # Way to merge two dicts
 # print(valid_data_dict1.update(valid_data_dict2))
 # print(final_dict)
 
+# Conversion to json object
 final_data = return_json(final_dict)
 print(final_data)
